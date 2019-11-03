0620486b833f363bdc466c92cf41ae718d45bde1
toyomorales <54047318+toyomorales@users.noreply.github.com>
2019-11-02 22:33:18 -0500

    Update Github3.java

--- src/github3/Github3.java
+++ src/github3/Github3.java
@@ -7,6 +7,7 @@ public class Github3 {
     public static void main(String[] args) {
         System.out.println("Hola es una práctica...");
         System.out.println("Se continua con la actualizacion ");
+     Systeme.out.print("Hola hola hola...");
     }
     
 }
